598534d85049678a5420da1597c44dd94e3a30f1
imcaleer <imcaleer@deloitte.co.uk>
2022-01-25 17:30:06 +0000

    update

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,7 +4,7 @@ module "sandbox" {
   control_tower_parameters = {
     AccountEmail              = "ctsandbox@outlook.com"
     AccountName               = "sandbox-aft"
-    ManagedOrganizationalUnit = "Sandbox (ou-doh2-kyck2cag)"
+    ManagedOrganizationalUnit = "Infrastructure"
     SSOUserEmail              = "ctsandbox@outlook.com"
     SSOUserFirstName          = "SandboxAccount"
     SSOUserLastName           = "AFT"

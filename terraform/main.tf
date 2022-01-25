a96262a550b4e4cb566651da26f3d03926c23570
imcaleer <imcaleer@deloitte.co.uk>
2022-01-25 14:23:38 +0000

    Create sandbox-aft Control Tower account

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,10 +2,10 @@ module "sandbox" {
   source = "./modules/aft-account-request"
 
   control_tower_parameters = {
-    AccountEmail              = "<ACCOUNT EMAIL>"
+    AccountEmail              = "ctsandbox@outlook.com"
     AccountName               = "sandbox-aft"
-    ManagedOrganizationalUnit = "Learn AFT"
-    SSOUserEmail              = "<SSO EMAIL>"
+    ManagedOrganizationalUnit = "Sandbox"
+    SSOUserEmail              = "ctsandbox@outlook.com"
     SSOUserFirstName          = "Sandbox"
     SSOUserLastName           = "AFT"
   }
